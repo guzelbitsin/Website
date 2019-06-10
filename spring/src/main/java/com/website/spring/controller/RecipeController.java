7c167a8f8caf95bb6f9a8cd951deb63b88cab03a
onurTe <onur.tetik@gmail.com>
2019-06-11 00:09:24 +0300

    logback created and welcome page opened

---
+++ spring/src/main/java/com/website/spring/controller/RecipeController.java
@@ -0,0 +1,19 @@
+package com.website.spring.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+public class RecipeController {
+
+    @ResponseBody
+    @GetMapping("welcome")
+    public String demo(){
+        log.info("demo methot called");
+        return "hello spring";
+    }
+
+}
