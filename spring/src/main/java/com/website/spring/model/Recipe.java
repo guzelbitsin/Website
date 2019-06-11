55f1be335e462a630779a663077201fb77527199
onurTe <onur.tetik@gmail.com>
2019-06-12 01:52:16 +0300

    some changes recipe class

--- spring/src/main/java/com/website/spring/model/Recipe.java
+++ spring/src/main/java/com/website/spring/model/Recipe.java
@@ -1,8 +1,12 @@
 package com.website.spring.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Setter
+@Getter
 public class Recipe {
     private int id;
     private String name;
