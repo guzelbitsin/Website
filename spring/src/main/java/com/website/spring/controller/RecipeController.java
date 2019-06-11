59f14a6f738bc8960455ece3926b8b884bc80ad8
onurTe <onur.tetik@gmail.com>
2019-06-12 00:55:41 +0300

    recipe controller and service defined some element.

--- spring/src/main/java/com/website/spring/controller/RecipeController.java
+++ spring/src/main/java/com/website/spring/controller/RecipeController.java
@@ -3,11 +3,19 @@ package com.website.spring.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
 public class RecipeController {
+
+    @ResponseBody
+    @RequestMapping("/")
+    public String home(){
+        // log.info("demo methot called");
+        return "hello spring";
+    }
 
     @ResponseBody
     @GetMapping("welcome")
