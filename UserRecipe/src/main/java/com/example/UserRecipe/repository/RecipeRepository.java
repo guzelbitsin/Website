054511bc6131301c50dc424f0007fdbf5414e5bc
onurTe <onur.tetik@gmail.com>
2019-07-14 22:45:50 +0300

    added UserRecipe

--- UserRecipe/src/main/java/com/example/UserRecipe/repository/RecipeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.UserRecipe.repository;
-
-import com.example.UserRecipe.domain.Recipe;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RecipeRepository extends CrudRepository<Recipe,Long> {
-       Iterable<Recipe> findByName(String name);
-}
--- UserRecipe/src/main/java/com/example/UserRecipe/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.UserRecipe.repository;
-
-import com.example.UserRecipe.domain.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    User findByUsername(String username);
-    User getUserByUsername(String username);
-}
