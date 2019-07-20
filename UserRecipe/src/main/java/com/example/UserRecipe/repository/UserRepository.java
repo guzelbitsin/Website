1c4c7db836c9f9a2d06dec33b2c91b40a028e345
onurTe <onur.tetik@gmail.com>
2019-07-20 12:28:56 +0300

    changed crud to jpa

--- UserRecipe/src/main/java/com/example/UserRecipe/repository/UserRepository.java
+++ UserRecipe/src/main/java/com/example/UserRecipe/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.example.UserRecipe.repository;
 
 import com.example.UserRecipe.domain.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
     User getUserByUsername(String username);
 }
