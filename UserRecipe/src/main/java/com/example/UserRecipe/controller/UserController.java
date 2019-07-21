bff12601142a21631845cb28f2d863d954491bef
onurTe <onur.tetik@gmail.com>
2019-07-21 21:54:33 +0300

    bugfixed some file

--- UserRecipe/src/main/java/com/example/UserRecipe/controller/UserController.java
+++ UserRecipe/src/main/java/com/example/UserRecipe/controller/UserController.java
@@ -3,10 +3,12 @@ package com.example.UserRecipe.controller;
 import com.example.UserRecipe.domain.User;
 import com.example.UserRecipe.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
